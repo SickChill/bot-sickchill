d76110eb8d3acdfc83afd4f3e3f3929efcc5f258
neoatomic <neoatomic@users.noreply.github.com>
2016-01-21 14:42:57 +0100

    Update issue.js
     Needs reviewing to make shore it doesn't break the bot.

--- configs/issue.js
+++ configs/issue.js
@@ -4,11 +4,30 @@ module.exports = {
     getNumber: function(data) {
         return data.issue.number;
     },
-    before: 'Thanks for the issue report! Before a real human comes by, please make sure your report has all the below criteria checked',
-    after: 'Please make sure you also read [how to create an issue](https://github.com/SickRage/sickrage-issues/blob/master/README.md) and followed all of the steps. \n\n' +
-        'The title should describe your issue. Having "SR not working" or "I get this bug" for 100 issues, isn\'t really helpful. We will close issues if there isn\'t enough information.\n\n' +
-        'Sometimes the devs may seem like grunts and respond with short answers. This isn\'t (always) because the dev hates you, but because he\'s on mobile or busy fixing bugs. If something isn\'t clear, please let us know, and this bot may get updated to automatically answer you.' +
-        '\n\nThanks!',
+    before: 'Thanks for the bug report! Please make sure you add all the information we require to investigate your issue. Be aware that we cant look into issues if there isn\'t enough information, and might close them as a result. More info can be found [here](https://github.com/SickRage/sickrage-issues/blob/master/README.md)',
+    after: ' For FEATURE REQUESTS use [Feathub](http://feathub.com/SickRage/SickRage)  \n\n' +
+            '  #### What info do we require? : \n\n' +
+            '- Branch \n\n' +
+            '- Commit hash \n\n' +
+            '- Your operating system & python version \n\n' +
+            '- Device (Synology, QNAP, PC etc.)  \n\n' +
+            '- What you did  \n\n' +
+            '- What happened \n\n' +
+            '- What are the steps to reproduce your issue? \n\n' +
+            '- Include a link with your **FULL** [log file](https://github.com/SiCKRAGE/sickrage-issues/wiki/FAQ\'s-and-Fixes#enable-debug-for-logs) that has the error. Please use [GIST](http://gist.github.com) or [Pastebin](http://pastebin.com/)  \n\n' +
+            '- Use a proper title. (The title should clearly describe your issue.)   \n\n' +
+            '- #### Many issues can simply be solved by: \n\n' +
+            '- Making sure you update & run to the latest version. \n\n' +
+            '- Have you tried turning your device off and on again? \n\n' +
+            '- Using the **search** function to see if this issue has already been reported/solved. \n\n' +
+            '- Do a [GIT reset](https://github.com/SiCKRAGE/sickrage-issues/wiki/FAQ\'s-and-Fixes#update-problems-try-this) - Checking the [ [Wiki] ](https://github.com/SiCKRAGE/sickrage-issues/wiki) for [ [Guides] ](https://github.com/SiCKRAGE/sickrage-issues/wiki/Installation-&-Configuration-Guides) [ [FAQS] ](https://github.com/SiCKRAGE/sickrage-issues/wiki/FAQ%27s-and-Fixes) [ [Main settings] ](https://github.com/SiCKRAGE/sickrage-issues/wiki/Settings-explained) [ [Show settings] ](https://github.com/SiCKRAGE/sickrage-issues/wiki/Show-settings-explained) [ [Remaining settings] ](https://github.com/SiCKRAGE/sickrage-issues/wiki/Remaining-settings-explained) \n\n' +
+            '- If SickRage doesnt find any episodes you probably need to :   \n\n' +
+            '   - Add a [Scene Exception](https://github.com/SiCKRAGE/sickrage-issues/wiki/Scene-exceptions-and-numbering) \n\n' +
+            '   - Make sure you understand how [quality detection](https://github.com/SiCKRAGE/sickrage-issues/wiki/Quality-Settings#quality-detectiondetermination) works during searches. \n\n' +
+            '   - Shutdown SickRage and remove the `cache.db` file \n\n' +
+            '- If you use a QNAP and have SSL errors try this [package](https://www.dropbox.com/s/j1svazqdi9ieq82/SickBeard-TVRage_151227.qpkg) & [info](http://forum.qnap.com/viewtopic.php?f=223&t=118366). \n\n' +
+            '- If you have a \'ascii codec can\'t encode character\' error, than set your Locale to [UTF-8](https://github.com/SiCKRAGE/sickrage-issues/wiki/FAQ%27s-and-Fixes#i-have-problems-with-special-characters-%C3%A9-etc-what-can-i-do) \n\n' +
+            '\n\nThanks!',
     checks: [
         // Basic info
         {
